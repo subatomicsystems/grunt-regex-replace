e5ec24fc9cd790efc0a45626fc4aed90d69bd164
Vlad Gurovich <vlad@subatomicsystems.com>
2013-12-02 16:54:27 -0800

    added a match function

--- tasks/regex-replace.js
+++ tasks/regex-replace.js
@@ -25,13 +25,17 @@ module.exports = function(grunt) {
       options = null,
       srchAction = null,
       rplAction = null,
+      matchAction = null,
+      originalContent,
       updatedContent;
       for(var i = 0; i< files.length; i++){
         if(toString.call(actions) === arrString){
           updatedContent = grunt.file.read(files[i]);
+          originalContent = updatedContent;
           for(var j = 0; j < actions.length; j++){
             srchAction = actions[j].search,
             rplAction = actions[j].replace; 
+            matchAction = actions[j].match;
             options = actions[j].flags;
             if(typeof options === 'undefined'){
               options = GLOBAL;
@@ -45,9 +49,22 @@ module.exports = function(grunt) {
             if(typeof rplAction === 'string'){
               rplAction = grunt.template.process(rplAction);
             }
-            updatedContent = regexReplace( updatedContent, srchAction, rplAction , options, j, actions[j].name);
+
+            if(typeof matchAction !== 'undefined' && typeof matchAction !== 'function' ) {
+              grunt.fail.fatal("Invalid type passed for \'match\': only functions accepted");
+            }
+            if(typeof matchAction === 'function') {
+              regexMatch(updatedContent, srchAction, matchAction, options);
+              if (typeof rplAction !== 'undefined') {
+                grunt.warn('Both \'match\' and \'replace\' actions were specified, but only one at a time is supported, so \'match\' was executed');
+              }
+            } else if (typeof rplAction !== 'undefined') {
+              updatedContent = regexReplace( updatedContent, srchAction, rplAction , options, j, actions[j].name);
+            }
           }
-          grunt.file.write(files[i], updatedContent);
+          if(updatedContent !== originalContent) {
+            grunt.file.write(files[i], updatedContent);  
+          }
           if(this.errorCount){
             return false;
           } 
@@ -58,7 +75,31 @@ module.exports = function(grunt) {
   // ==========================================================================
   // HELPERS
   // ==========================================================================
+ var regexMatch = function(src, regex, onMatch, options, index, actionName) {
 
+   if(typeof onMatch !== 'function') {
+    return;
+   }
+    var regExp = null, matches;
+
+    if(typeof regex ===  'string'){ 
+      regExp = new RegExp(regex , options); //regex => string
+    } else {
+      regExp = regex; //regex => RegExp object
+    }
+    matches = String(src).match(regExp);
+    for(var match in matches) {
+      onMatch.call(match);
+    }
+    index = typeof index === 'undefined' ? '' : index;
+    if(!actionName){
+      grunt.log.writeln(index + 1 + ' \'match\' action(s) completed.');
+    } else {
+      grunt.log.writeln(actionName + ' \'match\' action completed.');
+    }
+
+ };
+ 
  var regexReplace = function(src, regex, substr, options, index, actionName){
     //takes the src content and changes the content
     var regExp = null,
@@ -71,9 +112,9 @@ module.exports = function(grunt) {
     updatedSrc = String(src).replace(regExp, substr); //note: substr can be a function
     index = typeof index === 'undefined' ? '' : index;
     if(!actionName){
-      grunt.log.writeln(index + 1 + ' action(s) completed.');
+      grunt.log.writeln(index + 1 + ' \'replace\' action(s) completed.');
     } else {
-      grunt.log.writeln(actionName + ' action completed.');
+      grunt.log.writeln(actionName + ' \'replace\' action completed.');
     }
     return updatedSrc;
   };
